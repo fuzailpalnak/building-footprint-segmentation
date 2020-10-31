507ce09b3fff12ab2af49673bdc8cacfa5b3e8b4
Fuzail Palnak <fuzailpalnak@gmail.com>
2020-10-31 19:21:34 +0530

    function name in logger

--- building_segmentation/__init__.py
+++ building_segmentation/__init__.py
@@ -3,4 +3,4 @@ import logging
 logger = logging.getLogger("segmentation")
 logger.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s")
